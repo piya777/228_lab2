8b8867682106e083fad8ea3f274301ade2162ef5
Piya <piya.rat@ku.th>
2021-02-11 16:36:56 +0700

    6

--- 6320503068_2_4.c
+++ 6320503068_2_4.c
@@ -6,50 +6,54 @@
         int day;
         scanf("%c",&name);
         scanf("%d",&day);
-        if(day%2!=0)
+        if(day>0 && day<=31)
         {
-            if(day%10==5)
+            if(day%2!=0)
             {
-                printf("%c",92);
+                if(day%10==5)
+                {
+                    printf("%c",92);
+                }
+                if(name>='A' && name<='I')
+                {
+                    printf("(^_^)");
+                }
+                else if(name>='J' && name<='R')
+                {
+                    printf("(*o*)");
+                }
+                else if(name>='S' && name<='Z')
+                {
+                    printf("(T_T)");
+                }
+                if(day%10==5)
+                {
+                    printf("/");
+                }
             }
-            if(name>='A' && name<='I')
+            else if(day%2==0)
             {
-                printf("(^_^)");
-            }
-            else if(name>='J' && name<='R')
-            {
-                printf("(*o*)");
-            }
-            else if(name>='S' && name<='Z')
-            {
-                printf("(T_T)");
-            }
-            if(day%10==5)
-            {
-                printf("/");
+                if(day%10==5)
+                {
+                    printf("%c",92);
+                }
+                if(name>='A' && name<='I')
+                {
+                    printf("{@_@}");
+                }
+                else if(name>='J' && name<='R')
+                {
+                    printf("{*v*}");
+                }
+                else if(name>='S' && name<='Z')
+                {
+                    printf("{x_x}");
+                }
+                if(day%10==5)
+                {
+                    printf("/");
+                }
             }
         }
-        else if(day%2==0)
-        {
-            if(day%10==5)
-            {
-                printf("%c",92);
-            }
-            if(name>='A' && name<='I')
-            {
-                printf("{@_@}");
-            }
-            else if(name>='J' && name<='R')
-            {
-                printf("{*v*}");
-            }
-            else if(name>='S' && name<='Z')
-            {
-                printf("{x_x}");
-            }
-            if(day%10==5)
-            {
-                printf("/");
-            }
-        }
+
     }
