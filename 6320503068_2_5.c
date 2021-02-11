f0b8f93fd9569e59e75fbd6b88c11057fa48259d
Piya <piya.rat@ku.th>
2021-02-11 17:56:38 +0700

    10

--- 6320503068_2_5.c
+++ 6320503068_2_5.c
@@ -6,18 +6,18 @@
         scanf("%d %d",&day ,&month);
         switch(month)
         {
-            case 1: l=31;
-            case 2: l=28;
-            case 3: l=31;
-            case 4: l=30;
-            case 5: l=31;
-            case 6: l=30;
-            case 7: l=31;
-            case 8: l=31;
-            case 9: l=30;
-            case 10: l=31;
-            case 11: l=30;
-            case 12: l=31;
+            case 1: l=31;break;
+            case 2: l=28;break;
+            case 3: l=31;break;
+            case 4: l=30;break;
+            case 5: l=31;break;
+            case 6: l=30;break;
+            case 7: l=31;break;
+            case 8: l=31;break;
+            case 9: l=30;break;
+            case 10: l=31;break;
+            case 11: l=30;break;
+            case 12: l=31;break;
         }
        result=day+l;
        if(result>l)
--- 6320503068_2_6.c
+++ 6320503068_2_6.c
@@ -2,27 +2,28 @@
 
     void main()
     {
-        unsigned long long int N;
+
+        char num[15];
         int i;
-        scanf("%llu",&N);
-        if(N>=0 && N<=pow(10,1000000))
+        scanf("%s",num);
+        for(i=0;i<num[i];i++)
         {
-            while((N/10)>0)
+            switch(num[i])
             {
-                switch(N[i])
-                {
-                    case 0:printf("Zero");
-                    case 1:printf("One");
-                    case 2:printf("Two");
-                    case 3:printf("Three");
-                    case 4:printf("Four");
-                    case 5:printf("Five");
-                    case 6:printf("Six");
-                    case 7:printf("Seven");
-                    case 8:printf("Eight");
-                    case 9:printf("Nine");
-                }
-                N/=10;
+                case '0': printf("Zero"); break;
+                case '1': printf("One"); break;
+                case '2': printf("Two"); break;
+                case '3': printf("Three"); break;
+                case '4': printf("Four"); break;
+                case '5': printf("Five"); break;
+                case '6': printf("Six"); break;
+                case '7': printf("Seven"); break;
+                case '8': printf("Eight"); break;
+                case '9': printf("Nine"); break;
+            }
+            if(i<num[i+1])
+            {
+                printf("-");
             }
         }
 
